7c23e04346df25aceab3ec8390e6742807da7722
ronbp <ronmagpantay9513@gmail.com>
2021-07-11 22:37:50 -0700

    React Hooks, Event Handlers added. Partial form completion.

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 const Signup = () => {
+    let [name, setName] = useState('');
+    let [email, setEmail] = useState('');
+    let [password, setPassword] = useState('');
+    let [confirmPassword, setConfirmPassword] = useState('');
+    let [redirect, setRedirect] = useState(false);
+
+    const handleName = (e) => {
+        setName(e.target.value);
+    }
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const handleConfirmPassword = (e) => {
+        setConfirmPassword(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (password === confirmPassword) {
+            const newUser = { name, email, password }
+
+            axios.post(`${REACT_APP_SERVER_URL}/api/users/register`, newUser)
+            .then(response => {
+                console.log(response);
+                setRedirect(true);
+            })
+            .catch(error => console.log(error));
+        }
+    }
+    
+    if (redirect) return <Redirect to="login" />
+    // potentially place an alert here to notify user of invalid account parameters
+
     return (
         <div>
-            
+            <h2>Signup</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                </div>
+            </form>
         </div>
     );
 }
